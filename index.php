411f8d3bfb7c9d0d8a78180a787fb4932f5a68b3
Ganesh M <ganeshmdckap@gmail.com>
2023-05-18 17:10:16 +0530

    completed

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
 //$array = [5, 2, 1];
 //$array = [1, 2, 5];
 $array = [4, 1, 2, 5];
+
 $result = [];
 if (isDescendingOrder($array)) {
     echo "The array is already arranged in descending order.";
 } else {
     do {
-        for ($i = 0; $i < count($array) - 1; $i++) {
+        for ($i = count($array)-1; $i>=0; $i-- ) {
+            if ($i < count($array)-1) {
+                # code...
+            }
             if ($array[$i] < $array[$i + 1]) {
                 $temp = $array[$i + 1];
                 $array[$i + 1] = $array[$i];
@@ -24,9 +28,21 @@ if (isDescendingOrder($array)) {
 function isDescendingOrder($array)
 {
     for ($i = 0; $i < (count($array) - 1); $i++) {
-        if ($array[$i] < $array[$i + 1]) {
+        if ($array[$i + 1] > $array[$i]) {
             return false;
         }
     }
     return true;
 }
+
+
+
+// function isDescendingOrder($array)
+// {
+//     for ($i = 0; $i < (count($array) - 1); $i++) {
+//         if ( $array[$i] < $array[$i + 1]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
